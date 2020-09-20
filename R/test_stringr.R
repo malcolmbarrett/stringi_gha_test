7329447951b49ef7322ef75c4d59b409ae725da7
Malcolm Barrett <malcolmbarrett@gmail.com>
2020-09-20 10:31:11 -0700

    change pkg code

--- R/test_stringr.R
+++ R/test_stringr.R
@@ -1,6 +1,9 @@
 #' Test stringr
 #'
+#' @param msg a message
+#' @param msg a message
+#'
 #' @export
-test_stringr <- function() {
-  stringr::str_c('Letters again: ', letters)
+test_stringr <- function(msg) {
+  stringr::str_c(msg, letters)
 }
